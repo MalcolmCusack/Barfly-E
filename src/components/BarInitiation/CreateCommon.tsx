1fac82c28fa93bba85159d6a55b02e27ac35c7a9
Malcolm Cusack <cusackmalk@gmail.com>
2022-02-24 22:41:43 -0700

    publish branch

---
+++ src/components/BarInitiation/CreateCommon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CreateCommon() {
+  return <div></div>;
+}
+
+export default CreateCommon;
---
+++ src/components/BarInitiation/MultiStepForm.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MultiStepForm() {
+  return <div></div>;
+}
+
+export default MultiStepForm;
