e8a1a432348957a5586a920c5dcef2099cdead5e
Malcolm Cusack <cusackmalk@gmail.com>
2022-02-24 23:18:27 -0700

    multistep form logic created

--- src/components/BarInitiation/CreateCommon.tsx
+++ src/components/BarInitiation/CreateCommon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function CreateCommon() {
+function CreateCommon(props:any) {
   return <div></div>;
 }
 
--- src/components/BarInitiation/MultiStepForm.tsx
+++ src/components/BarInitiation/MultiStepForm.tsx
@@ -1,7 +1,58 @@
 import React from 'react';
+import CreateCommon from './CreateCommon';
+import CreateEmployees from './CreateEmployees';
+import CreateMenu from './CreateMenu';
+import { Button, Headline, TextInput } from "react-native-paper";
+import { Text, View } from '../../../components/Themed';
+
 
 function MultiStepForm() {
-  return <div></div>;
+    const [step, setStep] = React.useState(1);
+    const [payload, setPayload] = React.useState({
+        name: '',
+        email: '',  // awsEmail
+        phone: '',  // awsPhone 
+        location: {},   // json
+        payment: {},    // json
+        address: {}, //json
+        bio: ''
+    });
+
+    const nextStep = () => {
+        setStep(step + 1);
+    }
+
+    const prevStep = () => {
+        setStep(step - 1);
+    }
+
+    const handleInputData = input => e => {
+
+        const { value } = e.target
+
+        setPayload(prevState => ({
+            ...prevState,
+            [input]: value
+        }));
+    }
+
+    switch (step) {
+        case 1:
+            return (
+                <CreateCommon nextStep={nextStep} setPayload={setPayload} />
+            );
+        case 2:
+            return (
+                <CreateMenu nextStep={nextStep} prevStep={prevStep} setPayload={setPayload} />
+            );
+        case 3: 
+            return (
+                <CreateEmployees nextStep={nextStep} prevStep={prevStep} setPayload={setPayload} />
+            );
+        default:
+            return <></>
+    }
+
 }
 
 export default MultiStepForm;
